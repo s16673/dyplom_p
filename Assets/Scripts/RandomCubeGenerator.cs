a0f4cf85d92c2e72df32c97ad7588c3318738394
s16673 <56188105+s16673@users.noreply.github.com>
2020-03-04 21:10:49 +0100

    4_03_2020

--- Assets/Scripts/RandomCubeGenerator.cs
+++ Assets/Scripts/RandomCubeGenerator.cs
@@ -4,8 +4,7 @@ using System.Xml;
 using System.Xml.Serialization;
 using System.IO;
 using UnityEngine;
-
-
+using System.Runtime.Serialization.Formatters.Binary;
 
 public class RandomCubeGenerator : MonoBehaviour
 {
@@ -16,6 +15,11 @@ public class RandomCubeGenerator : MonoBehaviour
     public Vector3 gridOrigin = Vector3.zero;
     [XmlArray("mapa"), XmlArrayItem("klocek")]
     public GameObject[,] tab = new GameObject[10, 10];
+<<<<<<< HEAD
+    public int[,] tabint = new int[10, 10];
+    public bool serialized = false;
+=======
+>>>>>>> 3c187bba7fc6454513067a38dec514d8d6e6fa9e
     
 
     public void SpawnOnClick()
@@ -25,13 +29,43 @@ public class RandomCubeGenerator : MonoBehaviour
 
     public void SpawnGrid()
     {
-        for (int x = 0; x < gridX; x++)
-        {
-            for (int z = 0; z < gridZ; z++)
+        int randomIndex;
+        GameObject clone;
+        
+            for (int x = 0; x < gridX; x++)
             {
+<<<<<<< HEAD
+                for (int z = 0; z < gridZ; z++)
+                {
+                    Vector3 spawnPosition = new Vector3(x * gridSpacingOffset, 0, z * gridSpacingOffset) + gridOrigin;
+                    //tab[x, z] = PickAndSpawn(spawnPosition, Quaternion.identity);
+
+                    randomIndex = Random.Range(0, itemsToPickFrom.Length);
+                    tabint[x, z] = randomIndex;
+                    clone = Instantiate(itemsToPickFrom[randomIndex], spawnPosition, Quaternion.identity);
+                    clone.transform.parent = GameObject.Find("kontrolerMapy").transform;
+                    tab[x, z] = clone;
+                }
+=======
                 Vector3 spawnPosition = new Vector3(x * gridSpacingOffset, 0, z * gridSpacingOffset) + gridOrigin;
                 tab [x,z] = PickAndSpawn(spawnPosition, Quaternion.identity);
+>>>>>>> 3c187bba7fc6454513067a38dec514d8d6e6fa9e
             }
+        //XmlSerializer serializer = new XmlSerializer(typeof(GameObject));
+        //StreamWriter writer = new StreamWriter("mapa.xml");
+        //serializer.Serialize(writer.BaseStream, tab[0,0]);
+        //writer.Close();
+        if (!serialized)
+        {
+            //BinaryFormatter bf = new BinaryFormatter();
+            //MemoryStream ms = new MemoryStream();
+            //bf.Serialize(ms, tabint);
+            XMLOp.Serialize(tabint, "cubemap.xml");
+            serialized = true;
+        }
+        else 
+        {
+            Debug.Log("qwerty");
         }
     }
 
@@ -41,8 +75,11 @@ public class RandomCubeGenerator : MonoBehaviour
         GameObject clone = Instantiate(itemsToPickFrom[randomIndex], positionToSpawn, rotationToSpawn);
         clone.transform.parent = GameObject.Find("kontrolerMapy").transform;
         return clone;
+<<<<<<< HEAD
+=======
         
 
+>>>>>>> 3c187bba7fc6454513067a38dec514d8d6e6fa9e
     }
 }
 
