39fbe4b9a551955867598f872e9c7b9de8491f6f
s16673 <56188105+s16673@users.noreply.github.com>
2020-04-03 13:57:50 +0200

    save load xml

--- Assets/Scripts/RandomCubeGenerator.cs
+++ Assets/Scripts/RandomCubeGenerator.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Xml;
-using System.Xml.Serialization;
-using System.IO;
+//using System.Xml;
+//using System.Xml.Serialization;
 using UnityEngine;
-using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Xml.Serialization;
+
 
 public class RandomCubeGenerator : MonoBehaviour
 {
@@ -13,13 +14,9 @@ public class RandomCubeGenerator : MonoBehaviour
     public int gridZ;
     public float gridSpacingOffset = 1f;
     public Vector3 gridOrigin = Vector3.zero;
-    [XmlArray("mapa"), XmlArrayItem("klocek")]
+    public GameObject[] clonesPosition;
     public GameObject[,] tab = new GameObject[10, 10];
-<<<<<<< HEAD
-    public int[,] tabint = new int[10, 10];
-    public bool serialized = false;
-=======
->>>>>>> 3c187bba7fc6454513067a38dec514d8d6e6fa9e
+    public int[] tabint = new int[100];
     
 
     public void SpawnOnClick()
@@ -30,57 +27,37 @@ public class RandomCubeGenerator : MonoBehaviour
     public void SpawnGrid()
     {
         int randomIndex;
-        GameObject clone;
-        
-            for (int x = 0; x < gridX; x++)
+        XmlSerializer ser = new XmlSerializer(typeof(int[]));
+        TextWriter write = new StreamWriter("ser.xml");
+        for (int x = 0; x < gridX; x++)
+        {
+            for (int z = 0; z < gridZ; z++)
             {
-<<<<<<< HEAD
-                for (int z = 0; z < gridZ; z++)
-                {
-                    Vector3 spawnPosition = new Vector3(x * gridSpacingOffset, 0, z * gridSpacingOffset) + gridOrigin;
-                    //tab[x, z] = PickAndSpawn(spawnPosition, Quaternion.identity);
-
-                    randomIndex = Random.Range(0, itemsToPickFrom.Length);
-                    tabint[x, z] = randomIndex;
-                    clone = Instantiate(itemsToPickFrom[randomIndex], spawnPosition, Quaternion.identity);
-                    clone.transform.parent = GameObject.Find("kontrolerMapy").transform;
-                    tab[x, z] = clone;
-                }
-=======
                 Vector3 spawnPosition = new Vector3(x * gridSpacingOffset, 0, z * gridSpacingOffset) + gridOrigin;
-                tab [x,z] = PickAndSpawn(spawnPosition, Quaternion.identity);
->>>>>>> 3c187bba7fc6454513067a38dec514d8d6e6fa9e
+                randomIndex = Random.Range(0, itemsToPickFrom.Length);
+                tabint[x * 10 + z] = randomIndex;
+                GameObject clone = Instantiate(itemsToPickFrom[randomIndex], spawnPosition, Quaternion.identity);
+                clone.transform.parent = GameObject.Find("kontrolerMapy").transform;
+                tab[x, z] = clone;
             }
-        //XmlSerializer serializer = new XmlSerializer(typeof(GameObject));
-        //StreamWriter writer = new StreamWriter("mapa.xml");
-        //serializer.Serialize(writer.BaseStream, tab[0,0]);
-        //writer.Close();
-        if (!serialized)
-        {
-            //BinaryFormatter bf = new BinaryFormatter();
-            //MemoryStream ms = new MemoryStream();
-            //bf.Serialize(ms, tabint);
-            XMLOp.Serialize(tabint, "cubemap.xml");
-            serialized = true;
         }
-        else 
+        try
         {
-            Debug.Log("qwerty");
+            ser.Serialize(write, tabint);
+        }catch(System.Exception ex)
+        {
+            //TODO tag, ze blad xd 
         }
+        write.Close();
     }
 
+    /**
     public GameObject PickAndSpawn(Vector3 positionToSpawn, Quaternion rotationToSpawn)
     {
         int randomIndex = Random.Range(0, itemsToPickFrom.Length);
         GameObject clone = Instantiate(itemsToPickFrom[randomIndex], positionToSpawn, rotationToSpawn);
         clone.transform.parent = GameObject.Find("kontrolerMapy").transform;
         return clone;
-<<<<<<< HEAD
-=======
-        
-
->>>>>>> 3c187bba7fc6454513067a38dec514d8d6e6fa9e
-    }
+    }*/
 }
-
 
